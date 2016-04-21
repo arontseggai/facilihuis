180d7d8772a7835af083f64310dd91823fd395b1
Aron Tseggai <aron.tseggai@gmail.com>
2016-04-21 13:46:56 +0200

    adds role with owner, cleaner, admin or manager string. The default is owner

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419095611) do
+ActiveRecord::Schema.define(version: 20160421110549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160419095611) do
     t.string   "last_name"
     t.string   "telephone_number"
     t.string   "bank_account"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
