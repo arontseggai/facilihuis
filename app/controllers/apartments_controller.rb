fffebbcfa3e51773ccc9da82a7e577d0c337f375
Aron Tseggai <aron.tseggai@gmail.com>
2016-04-25 15:08:17 +0200

    this adds the ability to create new apartments

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,8 +3,9 @@ class ApartmentsController < ApplicationController
 
   def index
     @apartments = @user.apartments
+  end
 
-
+  def show
 
   end
 
@@ -12,9 +13,23 @@ class ApartmentsController < ApplicationController
     @apartment = Apartment.new
   end
 
+  def create
+    @apartment = Apartment.new(apartment_params)
+
+    if @apartment.valid?
+      @apartment.save
+      redirect_to user_apartments_path(current_user)
+    else
+      render "new"
+    end
+  end
 
 
   private
+
+  def apartment_params
+    params.require(:apartment).permit(:street, :user_id)
+  end
 
   def set_user
     @user = User.find(current_user)
