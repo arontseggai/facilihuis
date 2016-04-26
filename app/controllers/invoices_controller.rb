27c9e7996cee1f55788a37818125da8569e09458
Aron Tseggai <aron.tseggai@gmail.com>
2016-04-26 15:26:54 +0200

    works on setting routes for apartment invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,4 +1,5 @@
 class InvoicesController < ApplicationController
+  before_action :set_apartment, :set_user
   def index
     @invoices = @user.invoices
   end
@@ -7,8 +8,17 @@ class InvoicesController < ApplicationController
 
 
   private
+
+  def invoice_params
+    params.require(:invoices).permit(:apartment_id)
+  end
+
   def set_user
     @user = User.find(current_user)
+  end
+
+  def set_apartment
+    @apartment = @user.invoices.find(params[:apartment_id])
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :apartments
+    resources :invoices
   end
 end
